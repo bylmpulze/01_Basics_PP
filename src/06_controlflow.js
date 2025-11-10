43f2d51f1b299af4e6a88a6f4d3ba0ea36c38487
bylmpulze <steffen.maurer1993@gmail.com>
2025-11-10 08:57:04 +0100

    Add files via upload

---
+++ src/06_controlflow.js
@@ -0,0 +1,20 @@
+// Entscheidungsstrukturen | control flow
+
+// Deklaration + Assignment
+const ageJohn = 25;
+const ageMark = 30;
+
+// Deklaration
+let isJohnOlder, isJohnEqual;
+
+// Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
+
