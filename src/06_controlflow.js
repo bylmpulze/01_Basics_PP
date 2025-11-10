582be17cde869ff14e11bc5b394b6498ec71498f
bylmpulze <steffen.maurer1993@gmail.com>
2025-11-10 09:13:14 +0000

    .

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -1,20 +1,39 @@
-// Entscheidungsstrukturen | control flow
+// // Entscheidungsstrukturen | control flow
 
-// Deklaration + Assignment
-const ageJohn = 25;
+// // Deklaration + Assignment
+const ageJohn = 30;
 const ageMark = 30;
 
-// Deklaration
-let isJohnOlder, isJohnEqual;
+// // Deklaration
+// let isJohnOlder, isJohnEqual;
 
-// Test | Logische Aussage
+// // Test | Logische Aussage
 isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
-// Ausgabe
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
-console.log("------------------");
+// // Ausgabe
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+// // console.log("isJohnOlder: " + isJohnOlder);
+// // console.log("isJohnEqual: " + isJohnEqual);
+// console.log("------------------");
+
+// // if(true) Wahrheitswert
+// // if(false)
+// // if (isJohnOlder)
+// if (ageJohn >ageMark) //  direkter Test     
+// {
+//   console.log("John ist älter");  
+// // }
+// if (ageJohn > ageMark) {
+//       console.log("John ist älter"); 
+// } 
+// else 
+// {
+//       console.log("John ist jünger"); 
+// }
+
+// console.log( (isJohnOlder) ? "john is older" : "mark is older");
+
+
 
