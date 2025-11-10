121998e198f4e4c359f566d46714807e1c600f67
bylmpulze <steffen.maurer1993@gmail.com>
2025-11-10 10:51:48 +0000

    .

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -8,8 +8,8 @@ const ageMark = 30;
 // let isJohnOlder, isJohnEqual;
 
 // // Test | Logische Aussage
-isJohnOlder = (ageJohn > ageMark);
-isJohnEqual = (ageJohn == ageMark);
+// isJohnOlder = (ageJohn > ageMark);
+// isJohnEqual = (ageJohn == ageMark);
 
 // // Ausgabe
 // console.log("ageJohn: " + ageJohn);
@@ -35,5 +35,52 @@ isJohnEqual = (ageJohn == ageMark);
 
 // console.log( (isJohnOlder) ? "john is older" : "mark is older");
 
+// if (isJohnOlder) {
+//       console.log("John ist älter"); 
+// } 
+// else if(isJohnEqual)
+// {
+//       console.log("John ist genauso alt wie Marc");
+// }
+// else 
+// {
+//       console.log("John ist jünger"); 
+// }
+
+
+// const firstName = "jane";
+// // let job;
+
+// // job = "driver"; 
+// // // job = "diver";
+
+// // switch (job)
+// //    {
+// //       case "driver":
+// //             console.log("fährt taxi");
+// //             break;
+// //       case "diver":
+// //             console.log("Taucht im Rhein");
+// //             break;
+// //       default:
+// //             break;
+// //    }#
+
+/******** Fallunterscheidung / SWITCH|CASE 2 *******/
+
+const cond = true;
+const value = 10;
+
+switch (cond) {
+    case (value < 5):
+        console.log("Value hat den Wert 1.");
+        break;
+    case (value > 5):
+        console.log("Value hat den Wert 2.");
+        break;
+    default:
+        console.log("Value hat einen anderen Wert.");
+        break;
+}
 
 
